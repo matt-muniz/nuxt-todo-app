6238799c090b0eccbb08ba1f943ca72313b1b6a2
Matt Silva <mmunizsi01@gmail.com>
2024-12-23 17:12:24 -0500

    routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
+import { resolve } from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
   css: ["~/assets/main.css"],
   postcss: {
     plugins: {
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Welcome to Nuxt Todo App</h1>
+    <p>This is the home page of your Nuxt.js application.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #2c3e50;
+}
+p {
+  font-size: 18px;
+}
+</style>
