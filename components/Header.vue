11fea8166b967b1ae675fbc96c6bc2765182682a
Matt Silva <mmunizsi01@gmail.com>
2024-12-24 10:59:26 -0500

    working on header componnet style

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,12 @@
+<script setup>
+import Settings from '~/assets/settings.svg';
+import Search from '~/assets/search.svg';
+</script>
+
 <template>
-  <header class="bg-blue-500 text-white p-4">
-    <h1 class="text-2xl font-bold">Todo App</h1>
+  <header class="bg-blue-500 text-white p-4 text-center flex items-center justify-between">
+    <Settings class="text-[25px] md:text-[30px]" />
+    <h1 class="text-2xl font-bold">Dashboard</h1>
+    <Search class="text-[20px] md:text-[25px]" />
   </header>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,15 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  app: {
+    head: {
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "nuxt-svgo"],
   alias: {
     "@": resolve(__dirname, "/"),
   },
